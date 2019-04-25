7da99c1087dee159176d087bf68c1d73fde8ef0e
Fabio Bonaccorso <fabio.bonaccorso@roma2.infn.it>
2019-04-25 23:56:33 +0200

    Add small scripts(2)

--- test_cases/run_test/checkLast.sh
+++ test_cases/run_test/checkLast.sh
@@ -10,19 +10,21 @@ tests="2D_Poiseuille_gradP_xy 2D_Poiseuille_gradP_xz 2D_Poiseuille_gradP_yz 2D_P
 
 for i in $tests
 do
-	echo $i
-	cd $curDir; cd ../$i
-	grep "components" input.dat
-	grep "MD" input.dat
+	if [ -e output000000.map ]; then
+		echo $i
+		cd $curDir; cd ../$i
+		grep "components" input.dat
+		grep "MD" input.dat
 
-	echo "Diffs in output"
-	maxDiff.sh output000000.map output000000.map.orig 4 5 6 7 | awk '{ok[NR] = $4} END{PROCINFO["sorted_in"] = "@ind_num_asc"; OFMT="%14.3f"; for (i in ok) line = line ok[i] " "; print line}'
+		echo "Diffs in output"
+		maxDiff.sh output000000.map output000000.map.orig 4 5 6 7 | awk '{ok[NR] = $4} END{PROCINFO["sorted_in"] = "@ind_num_asc"; OFMT="%14.3f"; for (i in ok) line = line ok[i] " "; print line}'
 
-	if [ -e input.xyz ]; then
-		NF=$(head -n4 input.xyz | awk '{if ($1 =="C") print NF}' | head -n1)
-		echo "Diffs in restart.xyz"
-		maxDiff.sh restart.xyz restart.xyz.orig $(seq 2 $NF) | awk '{ok[NR] = $4} END{PROCINFO["sorted_in"] = "@ind_num_asc"; OFMT="%14.3f"; for (i in ok) line = line ok[i] " "; print line}'
+		if [ -e input.xyz ]; then
+			NF=$(head -n4 input.xyz | awk '{if ($1 =="C") print NF}' | head -n1)
+			echo "Diffs in restart.xyz"
+			maxDiff.sh restart.xyz restart.xyz.orig $(seq 2 $NF) | awk '{ok[NR] = $4} END{PROCINFO["sorted_in"] = "@ind_num_asc"; OFMT="%14.3f"; for (i in ok) line = line ok[i] " "; print line}'
+		fi
+
+		echo ""
 	fi
-
-	echo ""
 done
