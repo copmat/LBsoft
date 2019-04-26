f141fbbb1280d8c6ab1509cd4be72e3765490c86
Fabio Bonaccorso <fabio.bonaccorso@roma2.infn.it>
2019-04-26 17:36:54 +0200

    Add small scripts(4)

--- execute/maxDiff.sh
+++ execute/maxDiff.sh
@@ -4,11 +4,17 @@ file1=$1
 file2=$2
 
 shift; shift
+
+if [ "$1" == "-v" ]; then
+   verb=1
+   shift
+fi
+
 args=$*
 
 curDir=$(dirname $0)
 
 for i in $args
 do
-  $curDir/lineBylineDiff.sh $file1 $i $file2
+  $curDir/lineBylineDiff.sh $file1 $i $file2 $verb
 done
