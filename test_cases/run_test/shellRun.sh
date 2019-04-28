cdba0ccd8066b4bda83b40f073573854b202a47c
Fabio Bonaccorso <fabio.bonaccorso@iit.it>
2019-04-28 11:02:25 +0200

    Add small scripts(5): shellRun.sh is with relative path, Makefile with intel-mpidebugger

--- test_cases/run_test/shellRun.sh
+++ test_cases/run_test/shellRun.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-exe=/home/nodal2/IIT/LBsoft/execute/main_mpi.x
 
 curDir=$(pwd)
+exe=$curDir/../../execute/main_mpi.x
+
+ls -l $exe
 
 tests="2D_Poiseuille_gradP_xy 2D_Poiseuille_gradP_xz 2D_Poiseuille_gradP_yz 2D_Poiseuille_xy 2D_Poiseuille_xz 2D_Poiseuille_yz 2D_Shear_xy 2D_Shear_xz 2D_Shear_yz 3D_Spinodal 3D_Particle_pbc 3D_Rotating_Particle 3D_Shear_Particle 3D_Particles_box 3D_Particle_SC" 
 
