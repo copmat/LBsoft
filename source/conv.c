09b47c1d3d48098f6c51a34fa049c089499364d9
Fabio Bonaccorso <Fabio.bonaccorso@roma2.infn.it>
2019-06-29 13:16:10 +0200

    Tools for conversion HVAR dat->bin

--- source/conv.c
+++ source/conv.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 
+#define NUMFILES 1
+#define INIFILES 0
+#define STEPFILES 500
+
+#define NUMVAR 2
+
+
 int main(int argc, char **argv) {
-	int sz[3] = { 512,512,512};
+	int sz[3] = { 64,128,128};
 	size_t szPlane = sz[0] * sz[1];
 	double buf[szPlane];
 	char fname[1000];
 	int i, c, l;
 
 
-	for(i=0; i<20; i++) {
-	   sprintf(fname, "dumpHVAR.000%03d.000000.dat", (1+i)*10);
+	for(i=0; i<NUMFILES; i++) {
+	   int num = (1+i)*STEPFILES + INIFILES;
+
+	   sprintf(fname, "dumpHVAR.000%03d.000000.dat", num);
 	   printf("Converting:%s \n", fname);
 
 	   FILE *fp=fopen(fname,"rb");
-	   sprintf(fname, "out-%03d.bin", (1+i)*10);
+	   sprintf(fname, "out-%03d.bin", num);
 	   printf("Into      :%s \n", fname);
 	   FILE *fpOut=fopen(fname,"wb");
 	   if (!fp) return 2;
 	   if (!fpOut) return 3;
 
-	   for(l=0; l<5; l++) {
+	   for(l=0; l<NUMVAR; l++) {
 	      fseek(fp, 4, SEEK_CUR);
 	      for(c=sz[2]; c--; c>0) {
 	         fread (buf, sizeof(buf),1, fp);
--- source/convMPI.c
+++ source/convMPI.c
@@ -1,77 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void subcopy(double *dest, double *src, int i,int j,int k) {
-  int li,lj,lk, indexSrc=0;
+#define N 512
+#define PROCX 8
+#define PROCY 4
+#define PROCZ 4
 
-  for(li=0; li<64; li++) {
-    for(lj=0; lj<128; lj++) {
-      for(lk=0; lk<128; lk++) {
-	 int indexDest = (li + i) * 512*512 + (lj+j)*512 + lk+k;
-	 dest[indexDest] = src[indexSrc];
-         indexSrc++;
+#define NUMFILES 1
+#define INIFILES 0
+#define STEPFILES 500
+
+#define NUMVAR 2
+
+
+void subcopy(double *dest, double *src, int i, int j, int k, int ni,
+	     int nj, int nk)
+{
+  int li, lj, lk, indexSrc = 0;
+
+  for (lk = 0; lk < nk; lk++) {
+    for (lj = 0; lj < nj; lj++) {
+      for (li = 0; li < ni; li++) {
+	int indexDest = (lk + k) * N * N + (lj + j) * N + li + i;
+	dest[indexDest] = src[indexSrc];
+	indexSrc++;
       }
     }
   }
 }
 
-int main(int argc, char **argv) {
-	int sz[3] = {64,128,128}, readSz;
-	double *buf = NULL;
-	double *glob = NULL;
-	char fname[1000];
-	int i, c, l;
+int main(int argc, char **argv)
+{
+  int readSz;
+  double *buf[NUMVAR] = {NULL};
+  double *glob[NUMVAR] = {NULL};
+  char fname[1000];
+  int i, c, l;
+
+  const int lnx = N / PROCX;
+  const int lny = N / PROCY;
+  const int lnz = N / PROCZ;
 
 
-	for(l=0; l<1; l++) {
-	   glob = (double*) calloc( 512*512*512,sizeof(double) );
-	   buf = (double*) calloc( 64*128*128,sizeof(double) );
-	   if (buf==NULL || glob==NULL) {
-	      fprintf(stderr,"Mem alloc failed %x %x \n", glob, buf);
+  for (l = 0; l < NUMVAR; l++) {
+    glob[l] = (double *) calloc(N * N * N, sizeof(double));
+    buf[l] = (double *) calloc(lnx * lny * lnz, sizeof(double));
+    if (buf[l] == NULL || glob[l] == NULL) {
+      fprintf(stderr, "Mem alloc failed var:%d %x %x \n", l, glob, buf);
+      exit(4);
+    }
+  }
+
+  for (i = 0; i < NUMFILES; i++) {
+    int num = (1 + i) * STEPFILES + INIFILES;
+    int ic, jc, kc;
+
+    c = 0;
+    for (kc = 0; kc < PROCZ; kc++)
+      for (jc = 0; jc < PROCY; jc++)
+	for (ic = 0; ic < PROCX; ic++, c++) {
+
+	  sprintf(fname, "dumpHVAR.%06d.%06d.dat", num, c);
+	  printf("Converting:%s at:%d,%d,%d \n", fname, ic, jc, kc);
+
+	  FILE *fp = fopen(fname, "rb");
+	  if (!fp) {
+	    perror("fopen");
+	    exit(2);
+	  }
+
+	  for (l = 0; l < NUMVAR; l++) {
+	    fread(&readSz, sizeof(readSz), 1, fp);
+	    if (readSz != lnx * lny * lnz * sizeof(double)) {
+	      fprintf(stderr, "Found %d vs %d \n", readSz, sizeof(double) * lnx * lny * lnz);
 	      exit(4);
-	   }
+	    }
+	    readSz = fread(buf[l], sizeof(double), lnx * lny * lnz, fp);
+	    if (readSz != lnx * lny * lnz) {
+	      fprintf(stderr, "Read %d vs %d \n", readSz, sizeof(double) * lnx * lny * lnz);
+	      exit(5);
+	    }
+	    fseek(fp, 4, SEEK_CUR);
+
+	    subcopy(glob[l], buf[l], ic * lnx, jc * lny, kc * lnz, lnx, lny, lnz);
+	  }
+
+	  fclose(fp);
 	}
 
-	for(i=0; i<32; i++) {
-	   int num = (1+i)*500 + 10000;
-	   int ic,jc,kc;
+    for (l = 0; l < NUMVAR; l++) {
+      sprintf(fname, "out-var%d-%06d.bin", l, num);
+      printf("Into      :%s \n", fname);
+      FILE *fpOut = fopen(fname, "wb");
+      if (!fpOut) {
+	perror("fopen out file");
+	exit(2);
+      }
 
-	   c = 0;
-           for(kc=0; kc<4; kc++)
-             for(jc=0; jc<4; jc++)
-              for(ic=0; ic<8; ic++) {
+      fwrite(glob[l], N * N * N, sizeof(double), fpOut);
+      fclose(fpOut);
+    }
+  }
 
-	      sprintf(fname, "dumpHVAR.0%03d.000%03d.dat", num, c);
-	      printf("Converting:%s at:%d,%d,%d \n", fname, ic,jc,kc);
-
-	      FILE *fp=fopen(fname,"rb");
-	      if (!fp) return 2;
-
-	      for(l=0; l<1; l++) {
-	         fread (&readSz, sizeof(readSz),1, fp);
-	         if (readSz != 64*128*128*sizeof(double)) {
-	   	    fprintf(stderr,"Found %d vs %d \n", readSz, sizeof(double)*64*128*128);
-	   	    exit(4);
-	         }
-	         fread (buf, 64*128*128,sizeof(double), fp);
-	         fseek(fp, 4, SEEK_CUR);
-
-	         subcopy(glob, buf, ic*64,jc*128,kc*128);
-	      }
-
-	      fclose(fp);
-	      c++;
-	   }
-
-	   for(l=0; l<1; l++) {
-	       sprintf(fname, "out-var%d-%03d.bin", l,num);
-	       printf("Into      :%s \n", fname);
-	       FILE *fpOut=fopen(fname,"wb");
-	       if (!fpOut) return 3;
-	       fwrite (glob, 512*512*512,sizeof(double), fpOut);
-	       fclose(fpOut);
-	   }
-	}
-
-	return 0;
+  return 0;
 }
