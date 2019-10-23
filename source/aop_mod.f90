a34fd5828ba395b30917761a496be0b49ff4daab
Marco Lauricella <lauricella.marco@gmail.com>
2019-10-23 18:12:58 +0200

    ADD: banner added to aop_mod file

--- source/aop_mod.f90
+++ source/aop_mod.f90
@@ -2,6 +2,17 @@
 #include <default_macro.h>
 MODULE aop_mod
 
+!***********************************************************************
+!     
+!     LBsoft module containing definitions of pointer types in order
+!     to create structures of arrays
+!     
+!     licensed under Open Software License v. 3.0 (OSL-3.0)
+!     author: M. Bernaschi
+!     last modification October 2019
+!     
+!***********************************************************************
+
 TYPE REALPTR
    REAL(kind=PRC), pointer:: p(:,:,:)
 END TYPE REALPTR
